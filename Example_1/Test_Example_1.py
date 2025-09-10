ed1728d7d5f4c6a7fbfcdc1ce3090916c633cecc
Christopher Sharkey <ev2900@gmail.com>
2025-09-10 08:37:01 -0400

    added unit test

---
+++ Example_1/Test_Example_1.py
@@ -0,0 +1,16 @@
+# Unit tests for Example_1.py
+from fastapi.testclient import TestClient
+from Example_1 import app
+
+client = TestClient(app)
+
+# /
+def test_root():
+	response = client.get("/")
+	assert response.status_code == 200
+	assert response.json() == {"hello":"world"}
+
+# /about 
+def test_about():
+	response = client.get("/about")
+	assert response.status_code == 200
